bb02a0dba75e65af4eb4c1ece41dd3546c2e6f48
Jimmy Clairy <jimcl@hotmail.fr>
2023-01-22 10:42:29 +0100

    modif toggle nav & pictures

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const hamburgerButton = document.querySelector(".header__btn-toggle")
 const navigation = document.querySelector(".header__nav")
+const navLink = document.querySelectorAll(".header__nav__link")
 
 hamburgerButton.addEventListener("click", toggleNav)
+navLink.forEach(a => a.addEventListener("click", toggleNav))
 
 function toggleNav() {
     hamburgerButton.classList.toggle("active")
