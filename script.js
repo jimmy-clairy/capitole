3c342dfe2f5de780aaddf82a3a9be6cc446ae70d
Jimmy Clairy <jimcl@hotmail.fr>
2023-04-07 18:09:58 +0200

    add intersectionObserver

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function toggleNav() {
     hamburgerButton.classList.toggle("active")
     navigation.classList.toggle("active")
 }
+
+const ratio = .4
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio,
+};
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        } else {
+
+        }
+        console.log(entry.intersectionRatio);
+    })
+}
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('.reveal').forEach((r) => {
+    observer.observe(r)
+})
